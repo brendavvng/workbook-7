1068988ad27eca50cdb309921abedcb00d9dad2d
brendavvng <vangsbrenda@gmail.com>
2025-06-03 21:09:12 -0500

    Update q-7-CategoryID_and_AVG.sql

--- aggregates-and-grouping/q-7-CategoryID_and_AVG.sql
+++ aggregates-and-grouping/q-7-CategoryID_and_AVG.sql
@@ -1,3 +1,8 @@
+7. What is the category ID of each category and the average price of each item
+   in the category? You can answer this query by only looking at the Products table.
+  
+╭┈ • ┈ ୨୧ ┈ • ┈╮
+
 SELECT 
 CategoryID,
 AVG(UnitPrice)
@@ -5,3 +10,5 @@ FROM
 northwind.products
 GROUP BY
 CategoryID;
+
+╰┈ • ┈ ୨୧ ┈ • ┈╯
