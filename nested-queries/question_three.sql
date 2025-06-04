d4a92cb2c456cf132fdb0830e09934dd48212390
brendavvng <vangsbrenda@gmail.com>
2025-06-04 13:58:17 -0500

    Update question_three.sql

--- nested-queries/question_three.sql
+++ nested-queries/question_three.sql
@@ -1,3 +1,11 @@
+3. What are the order ids of the orders that ordered "Sasquatch Ale"? HINT:
+Find the product id of "Sasquatch Ale" in a subquery and then use that
+value to find the matching orders from the `order details` table. Because
+the `order details` table has a space in its name, you will need to surround
+it with back ticks in the FROM clause.
+
+┌─── ∘°💮°∘ ───┐
+
 SELECT 
 	OrderID,
 	ProductID
@@ -9,3 +17,5 @@ WHERE
         FROM northwind.products 
         WHERE ProductName = 'Sasquatch Ale'
         );
+
+└─── °∘💮∘° ───┘
