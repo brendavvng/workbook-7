476b41f523b4d705c05d11479cf374927303373c
brendavvng <vangsbrenda@gmail.com>
2025-06-03 21:09:49 -0500

    Update q-8-5Items_SupplierID_and_NumberOfItems.sql

--- aggregates-and-grouping/q-8-5Items_SupplierID_and_NumberOfItems.sql
+++ aggregates-and-grouping/q-8-5Items_SupplierID_and_NumberOfItems.sql
@@ -1,3 +1,9 @@
+8. For suppliers that provide at least 5 items to Northwind, what is the
+   supplier ID of each supplier and the number of items they supply? You
+   can answer this query by only looking at the Products table.
+
+╭┈ • ┈ ୨୧ ┈ • ┈╮
+
 SELECT 
 SupplierID,
 COUNT(*) AS NumberOfItems
@@ -7,3 +13,5 @@ GROUP BY
 SupplierID
 HAVING 
 COUNT(*) >= 5;
+
+╰┈ • ┈ ୨୧ ┈ • ┈╯
