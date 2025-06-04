ed65a0f01bc1ee805f15a2ee07374cc502f80fd6
brendavvng <vangsbrenda@gmail.com>
2025-06-03 21:07:56 -0500

    Update q-5-MostExpensiveItemPrice.sql

--- aggregates-and-grouping/q-5-MostExpensiveItemPrice.sql
+++ aggregates-and-grouping/q-5-MostExpensiveItemPrice.sql
@@ -1,4 +1,10 @@
+5. What is the price of the most expensive item that Northwind sells?
+  
+╭┈ • ┈ ୨୧ ┈ • ┈╮
+
 SELECT
 MAX(UnitPrice) as MostExpensiveItemPrice
 FROM 
 northwind.products;
+
+╰┈ • ┈ ୨୧ ┈ • ┈╯
