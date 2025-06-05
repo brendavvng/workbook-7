d19c1375f7b19ab576c51d449b3b60cc6f19afde
brendavvng <vangsbrenda@gmail.com>
2025-06-05 10:16:46 -0500

    Update question-2.sql

--- joins/question-2.sql
+++ joins/question-2.sql
@@ -1,8 +1,9 @@
 2. List the product id, product name, unit price and supplier name of all
    products that cost more than $75. Order by product name
 
+		 Query:
 ₊˚ ✧ ━━━━━━━━━━━━━⊱⋆⊰━━━━━━━━━━━━━ ✧ ₊˚
-
+	
 SELECT
 	P.ProductID,
     	P.ProductName,
