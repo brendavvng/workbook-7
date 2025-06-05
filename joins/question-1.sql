b0c525e22e7ab2e6ae1a850fe7c68a549f1bf89f
brendavvng <vangsbrenda@gmail.com>
2025-06-05 10:15:32 -0500

    Update question-1.sql

--- joins/question-1.sql
+++ joins/question-1.sql
@@ -1,3 +1,8 @@
+1. List the product id, product name, unit price and category name of all
+   products. Order by category name and within that, by product name.
+
+₊˚ ✧ ━━━━━━━━━━━━━⊱⋆⊰━━━━━━━━━━━━━ ✧ ₊˚
+	
 SELECT
 	P.ProductID,
     P.ProductName,
@@ -6,3 +11,5 @@ SELECT
 FROM
 	products P
     JOIN categories C ON (C.CategoryID = P.CategoryID);
+
+₊˚ ✧ ━━━━━━━━━━━━━⊱⋆⊰━━━━━━━━━━━━━ ✧ ₊˚
