03567a1a1a8f5699a59d10d8141c090bdac0c202
brendavvng <vangsbrenda@gmail.com>
2025-06-05 10:19:39 -0500

    Update question-3.sql

--- joins/question-3.sql
+++ joins/question-3.sql
@@ -6,15 +6,15 @@
 
 SELECT
 	P.ProductID,
-    P.ProductName,
-    P.UnitPrice,
-    C.CategoryName,
-    S.CompanyName AS SupplierName
+    	P.ProductName,
+    	P.UnitPrice,
+    	C.CategoryName,
+    	S.CompanyName AS SupplierName
 FROM
 	products P
-    JOIN categories C ON (P.CategoryID = C.CategoryID)
-    JOIN suppliers S ON (P.SupplierID = S.SupplierID)
+    	JOIN categories C ON (P.CategoryID = C.CategoryID)
+    	JOIN suppliers S ON (P.SupplierID = S.SupplierID)
 ORDER BY
-    P.ProductName;
+    	P.ProductName;
 
 ₊˚ ✧ ━━━━━━━━━━━━━⊱⋆⊰━━━━━━━━━━━━━ ✧ ₊˚
