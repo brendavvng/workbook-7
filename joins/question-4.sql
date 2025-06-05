1e66995a69ee3849f432f92205a43236acd33694
brendavvng <vangsbrenda@gmail.com>
2025-06-05 10:17:55 -0500

    Update question-4.sql

--- joins/question-4.sql
+++ joins/question-4.sql
@@ -1,22 +1,22 @@
 4. What is the product name(s) and categories of the most expensive
-products? HINT: Find the max price in a subquery and then use that in
-your more complex query that joins products with categories
+   products? HINT: Find the max price in a subquery and then use that in
+   your more complex query that joins products with categories
 
 		 Query:
 ₊˚ ✧ ━━━━━━━━━━━━━⊱⋆⊰━━━━━━━━━━━━━ ✧ ₊˚
 
 SELECT
 	P.ProductName,
-    P.UnitPrice,
-    C.CategoryID,
-    C.CategoryName
+    	P.UnitPrice,
+    	C.CategoryID,
+    	C.CategoryName
 FROM
 	products P
-    JOIN categories C ON (P.CategoryID = C.CategoryID)
+    	JOIN categories C ON (P.CategoryID = C.CategoryID)
 WHERE
 	P.UnitPrice = (
-    SELECT MAX(UnitPrice) 
-    FROM Products
-    );
+    	SELECT MAX(UnitPrice) 
+    	FROM Products
+    	);
 
 ₊˚ ✧ ━━━━━━━━━━━━━⊱⋆⊰━━━━━━━━━━━━━ ✧ ₊˚
