494cd633adea899d489591ac12b5cbc61a943e81
brendavvng <vangsbrenda@gmail.com>
2025-06-05 10:16:06 -0500

    Update question-2.sql

--- joins/question-2.sql
+++ joins/question-2.sql
@@ -1,12 +1,19 @@
+2. List the product id, product name, unit price and supplier name of all
+   products that cost more than $75. Order by product name
+
+₊˚ ✧ ━━━━━━━━━━━━━⊱⋆⊰━━━━━━━━━━━━━ ✧ ₊˚
+
 SELECT
 	P.ProductID,
-    P.ProductName,
-    P.UnitPrice,
-    S.CompanyName AS SupplierName
+    	P.ProductName,
+    	P.UnitPrice,
+    	S.CompanyName AS SupplierName
 FROM
 	products P
-    JOIN suppliers S ON (P.SupplierID = S.SupplierID)
+    	JOIN suppliers S ON (P.SupplierID = S.SupplierID)
 WHERE
-    P.UnitPrice > 75
+    	P.UnitPrice > 75
 ORDER BY
-    P.ProductName;
+    	P.ProductName;
+
+₊˚ ✧ ━━━━━━━━━━━━━⊱⋆⊰━━━━━━━━━━━━━ ✧ ₊˚
