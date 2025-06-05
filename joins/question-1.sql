eb58a99447e9fbfcf720f72b6bcca8a70361ae06
Brenda Vang <vangsbrenda@gmail.com>
2025-06-05 10:38:18 -0500

    reuploading question 1, forgot to add ORDER BY

--- joins/question-1.sql
+++ joins/question-1.sql
@@ -1,23 +1,11 @@
-1. List the product id, product name, unit price and category name of all
-   products. Order by category name and within that, by product name.
-
-		 Query:
-₊˚ ✧ ━━━━━━━━━━━━━⊱⋆⊰━━━━━━━━━━━━━ ✧ ₊˚
-	
 SELECT
 	P.ProductID,
-    	P.ProductName,
-    	P.UnitPrice,
-    	C.CategoryName
+    P.ProductName,
+    P.UnitPrice,
+    C.CategoryName
 FROM
 	products P
-<<<<<<< HEAD
-    	JOIN categories C ON (C.CategoryID = P.CategoryID);
-
-₊˚ ✧ ━━━━━━━━━━━━━⊱⋆⊰━━━━━━━━━━━━━ ✧ ₊˚
-=======
     JOIN categories C ON (C.CategoryID = P.CategoryID)
 ORDER BY
 	C.CategoryName,
     P.ProductName;
->>>>>>> a352bfb (forgot to add in the Order By in question 1)
