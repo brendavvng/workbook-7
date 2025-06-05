5d806fb28e97b28e8f422318904b3283d3a41da1
brendavvng <vangsbrenda@gmail.com>
2025-06-05 10:38:56 -0500

    Update question-1.sql

--- joins/question-1.sql
+++ joins/question-1.sql
@@ -1,11 +1,19 @@
+1. List the product id, product name, unit price and category name of all
+   products. Order by category name and within that, by product name.
+
+		 Query:
+₊˚ ✧ ━━━━━━━━━━━━━⊱⋆⊰━━━━━━━━━━━━━ ✧ ₊˚
+
 SELECT
 	P.ProductID,
-    P.ProductName,
-    P.UnitPrice,
-    C.CategoryName
+    	P.ProductName,
+    	P.UnitPrice,
+    	C.CategoryName
 FROM
 	products P
-    JOIN categories C ON (C.CategoryID = P.CategoryID)
+    	JOIN categories C ON (C.CategoryID = P.CategoryID)
 ORDER BY
 	C.CategoryName,
-    P.ProductName;
+    	P.ProductName;
+
+₊˚ ✧ ━━━━━━━━━━━━━⊱⋆⊰━━━━━━━━━━━━━ ✧ ₊˚
